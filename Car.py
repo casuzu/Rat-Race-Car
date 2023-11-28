0785e4a5d2459f3a4ae686b0c0027374f213fcd4
delers447 <45727495+delers447@users.noreply.github.com>
2023-11-27 21:26:26 -0500

    Add files via upload

--- Car.py
+++ Car.py
@@ -1,8 +1,17 @@
 
 #def __func() denotes private function anything else is public. Same for variables.
 # e.g. __x is private
+
+#Asuzu's Imports
 import numpy as np
 import math as m
+
+#Dan's Imports
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Activation, Reshape
 
 def WIDTH():
     carWidth = 30
@@ -13,12 +22,13 @@ def LENGTH():
     return carLength
 
 def RADIANS(deg):
-    deg = deg % 360  # normalize car value in degrees.
+    deg = deg % 360  # convert car value in degrees.
     theta = deg * m.pi/ 180.0
     return theta
 
 def THETA_INCREMENT(): # the value by the car turn if it is to turn. 20 is preferred.
     return 20.0
+
 def MAX_SPEED():
     return 200 # 50 is standard max car speed.
 
@@ -27,22 +37,30 @@ def TIME(): # the time it takes to reach the full car speed.
 
 # Ideally a precise set of driving instruction per frame.
 actions_1 =np.array([
-                            ["Straight", "Accelerate"], ["Straight", "Accelerate"],
-                            ["Straight", "Accelerate"], ["Straight", "Accelerate"],
-                            ["Right", "Brake"], ["Right", "Brake"],
-                            ["Straight", "Accelerate"],["Straight", "Coast"],
-                            ["Straight", "Brake"],["Right", "Brake"],
-                            ["Right", "Brake"], ["Straight", "Accelerate"]
-                             ])
+                            ["Straight", "Accelerate"], 
+                            ["Straight", "Accelerate"],
+                            ["Straight", "Accelerate"], 
+                            ["Straight", "Accelerate"],
+                            ["Right", "Brake"], 
+                            ["Right", "Brake"],
+                            ["Straight", "Accelerate"],
+                            ["Straight", "Coast"],
+                            ["Straight", "Brake"],
+                            ["Right", "Brake"],
+                            ["Right", "Brake"], 
+                            ["Straight", "Accelerate"]
+                            ])
 class CarObj:
 
     # tire positions on car
+    #     FRONT?
     # t[0]----t[1]
     # |         |
     # |  Car    |
     # |  Shape  |
     # |         |
     # t[2]----t[3]
+    #     BACK?
 
     __center = np.zeros(2).reshape(1, 2) # center of the car [x, y] position size: [1x2].
     __tires = np.zeros(8).reshape(4, 2) # list of tires [x, y] positions size: [4x1x2].
@@ -65,10 +83,160 @@ class CarObj:
         # returns the start position of the front tires at the start of the race.
         self.__car_start_pos = self.front_bumper_pos.copy()
 
+        #Dan's Additions for Learning.
+        self.action_space = [
+                            [[1,0,0],[1,0,0]], [[1,0,0],[0,1,0]], [[1,0,0],[0,0,1]],
+                            [[0,1,0],[1,0,0]], [[0,1,0],[0,1,0]], [[0,1,0],[0,0,1]],
+                            [[0,0,1],[1,0,0]], [[0,0,1],[0,1,0]], [[0,0,1],[0,0,1]]
+                            ]
+        #Hyper-parameters for the "Deep Q Learning"
+        self.gamma= 0.99
+        self.epsilon = 1.0
+        self.epsilon_dec = 0.99
+        self.epsilon_min = 0.10
+        self.batch_size = 64
+        self.memory = ReplayBuffer(1_000_000, 8, 12, discrete=True)
+        #Randomly Seeded Neural Network
+        self.create_new_model() 
+
     def update_car_history(self):
         self.front_bumper_history.append(self.front_bumper_pos.copy())
         self.tires_history.append([self.__tires[0].copy(), self.__tires[1].copy(),
                                        self.__tires[2].copy(), self.__tires[3].copy()])
+
+    def create_new_model(self):
+        """
+        Added by Dan.  
+        Create a new randomly seeded model.
+        """
+
+        model = Sequential()
+
+        #Input Layer.  
+        model.add(Input(shape=(8, )))
+
+        #First Hidden Layer
+        model.add(Dense(64, activation='sigmoid'))
+
+        #Second Hidden Layer
+        model.add(Dense(64, activation='sigmoid'))
+
+        #Output Layer
+        model.add(Dense(2*3))
+        model.add(Reshape((-1, 3)))
+        model.add(Activation('softmax'))
+
+        #Package the model
+        model.compile(
+                loss='mse', 
+                optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3))
+        model.summary()
+
+        self.model = model
+
+    def save_model(self, name):
+        """
+        Written by Dan
+        Save the model using a name. Will save in the current working directory.
+        """
+        print(F"Saving the model: {name}.model ...")
+        try:
+            self.model.save(F"{name}.model")
+            print(F"The model saved successfully.")
+        except:
+            print(F"ERROR: The model did not save.")
+            assert False
+
+    def load_model(self, name):
+        """
+        written by Dan.
+        Load the name of the model.  THe model should be in the current working directory.
+        {name}.model
+        """
+        print(F"Loading the model: {name}.model ...")
+        try:
+            self.model = tf.keras.models.load_model(F"{name}.model")
+            print(F"The model loaded successfully.")
+        except:
+            print(F"ERROR: The model did not load.")
+            assert False
+
+    def make_decision(self, state):
+        """
+        Written by Dan.  
+        state is a tuple of 8 values:
+        Distances in the following directions:
+            N, NE, E, SE, S, SW, W, NW
+        Car will pick the best action based on state.
+        100% explotation with ZERO exploration.  ie, not Explore()
+        
+        Output is tuple of strings, like: ['Right', 'Accelerate']
+        """
+        print(F"The model is making a decision based on {state}")
+        outputs = self.model.predict([state])
+        outputs = outputs[0]
+        #print(F"The ouput of the model is {outputs}.")
+
+        decision = []
+
+        #print(outputs[0])
+        #print(outputs[1])
+
+        #print(F"The max of the first tuple is {max(outputs[0])}.")
+        #print(F"The max of the second tuple is {max(outputs[1])}.")
+
+        first_max = max(outputs[0])
+        if outputs[0][0] == first_max:
+            decision.append("Left")
+        elif outputs[0][1] == first_max:
+            decision.append("Straight")
+        elif outputs[0][2] == first_max:
+            decision.append("Right")
+        else:
+            decision.append("ERROR")
+
+        second_max = max(outputs[1])
+        if outputs[1][0] == second_max:
+            decision.append("Accelerate")
+        elif outputs[1][1] == second_max:
+            decision.append("Coast")
+        elif outputs[1][2] == second_max:
+            decision.append("Brake")
+        else:
+            decision.append("ERROR")
+
+        print(F"The decision is: {decision}.")
+        return decision
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+
+    def explore(self, state):
+        """
+        Written by Dan.
+        Car picks the next action with threshold of random choices to factiltate exploration.
+        make_decision is 0% exploration and 100% exploitation while
+        Explore() is Epislon% Expliration and 100-Epsilon % exploitation.
+
+        Output is 1 hot matrix, like [[1,0,0],[0,1,0]]
+        """
+        state = state[np.newaxis, :]
+        rand = np.random.random()
+        if rand < self.epsilon:
+            action = np.random.choice(self.action_space)
+        else: 
+            outcome = self.model.predict(state)
+            action = [[0,0,0],[0,0,0]]
+            action[np.argmax(outcome[0])] = 1
+            action[np.argmax(outcome[1])] = 1
+        return action
+
+    def learn(self):
+        """
+        Written by Dan.
+        TBD
+        """
+        pass
 
     def update_Carposition(self): # calculate and update the car's center and tires.
         #[0][0][Ft1]----------[0][1][Ft2]
@@ -212,8 +380,6 @@ class CarObj:
         print(F'Car Speed = {self.__speed}')
 
 
-
-
     def run(self):
 
         for i, element in enumerate(actions_1):
@@ -224,3 +390,62 @@ class CarObj:
 
     #def NN_interim(self, radar_info):
      #   if (m.absradar_info[1])== :
+
+
+class ReplayBuffer:
+    def __init__(self, max_size, input_shape, n_actions, discrete=False):
+        #Reference: https://www.youtube.com/watch?v=5fHngyN8Qhw
+        #Referemce: https://github.com/philtabor/Youtube-Code-Repository/blob/master/ReinforcementLearning/DeepQLearning/simple_dqn_keras.py
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.discrete = discrete
+        self.state_memory = np.zeros((self.mem_size, input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, input_shape))
+        dtype = np.int8 if self.discrete else np.float32
+        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=dtype)
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        #Store one hot encoding of actions, if appropiate.
+        #Dan Change this latter to [[0,0,1],[1,0,0]] (ie, 2x3 one of zeroes)
+        if self.discrete:  
+            actions = np.zeros(self.action_memory.shape[1])   
+            actions[action] = 1.0
+            self.action_memory[index] = actions
+        else:
+            self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory = 1 - done
+        self.mem_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        states_ = self.new_state_memory[batch]
+        terminal = self.terminal_memory[batch]
+
+        return states, actions, rewards, states_, terminal
+
+
+if __name__ == "__main__":
+    
+    #Example of the code to work with Neural network.
+    print(F"The program started successfully.")
+    Dan_Car = CarObj("Dan", [200, 30], 90)
+    print(F"The car object has been made.")
+    Dan_Car.create_new_model()
+    #Dan_Car.load_model("example")
+    print(F"A new model has been made.")
+    print(F"Making a decision ...")
+    Dan_Car.make_decision((10, 10, 10, 10, 10, 10, 10, 10)) #distance of 10 in all 8 directions.
+    print(F"Saving the model ...")
+    Dan_Car.save_model("example")
+    print(F"The model saved.")
